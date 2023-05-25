c1d637a2e3f7fe369d67545302d9b5984625cadd
Alferova Ekaterina <User@Administrators-MacBook-Pro.local>
2023-05-25 19:28:44 +0300

    Больше подробностей (часть 2)

---
+++ project/src/components/overview/overview.tsx
@@ -0,0 +1,24 @@
+import { FilmData } from '../../types/films';
+
+type OverviewProp = {
+  film: FilmData | undefined;
+};
+
+const Overview = ({film}: OverviewProp) => (
+  <>
+    <div className="film-rating">
+      <div className="film-rating__score">{film?.rating}</div>
+      <p className="film-rating__meta">
+        <span className="film-rating__level">Very good</span>
+        <span className="film-rating__count">{film?.scoresCount} ratings</span>
+      </p>
+    </div>
+    <div className="film-card__text">
+      <p>{film?.description}</p>
+      <p className="film-card__director"><strong>Director: {film?.director}</strong></p>
+      <p className="film-card__starring"><strong>Starring: {film?.starring.join(', ')} and other</strong></p>
+    </div>
+  </>
+);
+
+export default Overview;
--- project/src/const.ts
+++ project/src/const.ts
@@ -11,3 +11,5 @@ export const AppRoutes = {
 export const RATING_STARS = 10;
 export const REVIEW_MIN_LENGTH = 50;
 export const REVIEW_MAX_LENGTH = 400;
+
+export const FILM_NAV = ['Overview', 'Details', 'Reviews'];
